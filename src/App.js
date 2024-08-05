67efc7e2f5fc36c939c1f92b37985708d082dc30
Chrishore Thomas <chrishore.thomas@infosys.com>
2024-08-05 12:37:36 +0530

    Added new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
+import 'bulma/css/bulma.css';
 import logo from './logo.svg';
 import './App.css';
+import './Navbar';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Title</h1>
+        <Navbar></Navbar>
     </div>
   );
 }
