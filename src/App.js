1336926ed8e4648840f915d8e18110919c9ec5e3
Chrishore Thomas <chrishore.thomas@infosys.com>
2024-08-05 12:44:20 +0530

    new addign

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import 'bulma/css/bulma.css';
+import logo from './logo.svg';
+import './App.css';
+import './Navbar';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Title</h1>
+        <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
